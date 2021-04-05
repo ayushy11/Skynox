55f13541dc5282464f6f0d57e0624ff28670f13b
Ayush Yadav <ayushexx@gmail.com>
2021-04-05 21:28:05 +0530

    Initialcommit

---
+++ script.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('.img-select a');
+const imgButtons = [...images];
+let imgId = 1;
+
+// onClick event listener for each image.
+imgButtons.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+// slide to the selected image
+function slideImage(){
+    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+window.addEventListener('resize', slideImage);
